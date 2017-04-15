682ef0480ee66de4e642ef0b748ac783121fec11
Dave Pinchoff <dpinchoff@gmail.com>
2017-04-15 21:44:24 +0000

    minor updates

--- Hashes/06_anagrams.rb
+++ Hashes/06_anagrams.rb
@@ -7,7 +7,7 @@ words.each { |word| sorted_words << word.chars.sort.join }
 sorted_words.uniq!
 
 sorted_words.each do |first| 
-  p words.select { |second| second.chars.sort.join == first }
+  p words.select { |second| second.chars.sort.join == first } #This line uses .select to return an array of every word in [words] which, when letters are alphabetical in a String, are equal to the already alphabetical array of Strings we have in sorted_words, whose element is accessed by the first variable.
 end
 
 
